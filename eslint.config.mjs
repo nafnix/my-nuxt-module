fcfb1393cb2e48b2c81d385603b10dd95a3d90c7
nafnix <nafnix@localhost.localdomain>
2024-07-05 11:18:53 +0800

    chore: 第一次提交

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+import antfu from '@antfu/eslint-config'
+
+// Run `npx @eslint/config-inspector` to inspect the resolved config interactively
+export default createConfigForNuxt({
+  features: {
+    // Rules for module authors
+    tooling: true,
+    // Rules for formatting
+    stylistic: true,
+  },
+  dirs: {
+    src: [
+      './playground',
+    ],
+  },
+})
+  .append(
+    antfu(
+      {
+        formatters: true,
+        typescript: true,
+      },
+    ),
+  )
